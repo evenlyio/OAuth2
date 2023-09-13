d114cb8c24e1bcd89eccb2a6798a85f2ddde2ffb
Martin Pittenauer <map@evenly.io>
2023-09-13 15:41:44 +0200

    Adaptions for visionOS

--- Sources/tvOS/OAuth2CustomAuthorizer+tvOS.swift
+++ Sources/tvOS/OAuth2CustomAuthorizer+tvOS.swift
@@ -17,7 +17,7 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 //
-#if os(tvOS)
+#if os(tvOS) || os(visionOS)
 
 import Foundation
 import UIKit
