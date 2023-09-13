2a7e28227cccbe9625cf4976ac8f9d3774c27273
Martin Pittenauer <map@evenly.io>
2023-09-13 15:41:44 +0200

    Adaptions for visionOS

--- Sources/iOS/OAuth2Authorizer+iOS.swift
+++ Sources/iOS/OAuth2Authorizer+iOS.swift
@@ -17,7 +17,7 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 //
-#if os(iOS) || os(visionOS)
+#if os(iOS)
 
 import UIKit
 import SafariServices
